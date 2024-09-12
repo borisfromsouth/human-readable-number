611736c33ae4b8d9128c6f7385544a84e201a21d
Alexander Borisenko <sashaborisenko@tut.by>
2024-09-12 11:56:01 +0300

    fix: correct string return

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ let hundreds_res = "";
 let thousands_res = "";
 
 let numberString = number.toString();
+
 if(numberString.length === 4){
     units = numberString[3];
     tens = numberString[2];
@@ -32,178 +33,147 @@ if(numberString.length === 1){
     units = numberString[0];
 }
 
-/*if(number / 1000 !== 0) {
-thousands = number / 1000;
-number -= thousands * 1000;
-}
-if(number / 100 !== 0) {
-    hundreds = number / 100;
-    number -= hundreds * 100;
-}*/
-/*if(number / 10 !== 0) {
-    tens = number / 10;
-    number -= tens * 10;
-}
-units = number;*/
-
-switch(units){
+switch(thousands){
     case '1':
-        units_res = "one";
+        thousands_res = "one thousand ";
         break;
-    case '2':
-        units_res = "two";
-        break;
-    case '3':
-        units_res = "three";
-        break;
-    case '4':
-        units_res = "four";
-        break;
-    case '5':
-        units_res = "five";
-        break;
-    case '6':
-        units_res = "six";
-        break;
-    case '7':
-        units_res = "seven";
-        break;
-    case '8':
-        units_res = "eight";
-        break;
-    case '9':
-        units_res = "nine";
-        break;
-    case '0':
-        units_res = "zero";
-        break;
-}
-
-/*switch(thousands){
-    case 1:
-        thousands = "one thousand";
-        break;
-    case 2:
-        thousands = "two thousand";
-        break;
-    case 3:
-        thousands = "three thousand";
-        break;
-    case 4:
-        thousands = "four thousand";
-        break;
-    case 5:
-        thousands = "five thousand";
-        break;
-    case 6:
-        thousands = "six thousand";
-        break;
-    case 7:
-        thousands = "seven thousand";
-        break;
-    case 8:
-        thousands = "eight thousand";
-        break;
-    case 9:
-        thousands = "nine thousand";
+    default:
+        thousands_res = "";
         break;
 }
 
 switch(hundreds){
-    case 1:
-        hundreds = "one hundreds";
+    case '1':
+        hundreds_res = "one hundred ";
         break;
-    case 2:
-        hundreds = "two hundreds";
+    case '2':
+        hundreds_res = "two hundred ";
         break;
-    case 3:
-        hundreds = "three hundreds";
+    case '3':
+        hundreds_res = "three hundred ";
         break;
-    case 4:
-        hundreds = "four hundreds";
+    case '4':
+        hundreds_res = "four hundred ";
         break;
-    case 5:
-        hundreds = "five hundreds";
+    case '5':
+        hundreds_res = "five hundred ";
         break;
-    case 6:
-        hundreds = "six hundreds";
+    case '6':
+        hundreds_res = "six hundred ";
         break;
-    case 7:
-        hundreds = "seven hundreds";
+    case '7':
+        hundreds_res = "seven hundred ";
         break;
-    case 8:
-        hundreds = "eight hundreds";
+    case '8':
+        hundreds_res = "eight hundred ";
         break;
-    case 9:
-        hundreds = "nine hundreds";
+    case '9':
+        hundreds_res = "nine hundred ";
         break;
-}*/
+    default:
+        hundreds_res = "";
+        break;
+}
 
 switch(tens){
     case '1':
         switch(units){
             case '1':
-                tens_res = "eleven";
+                tens_res = "eleven ";
                 break;
             case '2':
-                tens_res = "twelve";
+                tens_res = "twelve ";
                 break;
             case '3':
-                tens_res = "thirteen";
+                tens_res = "thirteen ";
                 break;
             case '4':
-                tens_res = "fourteen";
+                tens_res = "fourteen ";
                 break;
             case '5':
-                tens_res = "fifteen";
+                tens_res = "fifteen ";
                 break;
             case '6':
-                tens_res = "sixteen";
+                tens_res = "sixteen ";
                 break;
             case '7':
-                tens_res = "seventeen";
+                tens_res = "seventeen ";
                 break;
             case '8':
-                tens_res = "eighteen";
+                tens_res = "eighteen ";
                 break;
             case '9':
-                tens_res = "nineteen";
+                tens_res = "nineteen ";
+                break;
             case '0':
-                tens_res = "ten";
+                tens_res = "ten ";
                 break;
         }
-        return (thousands_res + " " + hundreds_res + " " + tens_res /*+ " " + units_res*/).trim();
+        return (thousands_res  + hundreds_res  + tens_res  + units_res).trim();
     case '2':
-        tens_res = "twenty";
+        tens_res = "twenty ";
         break;
     case '3':
-        tens_res = "thirty";
+        tens_res = "thirty ";
         break;
     case '4':
-        tens_res = "fourty";
+        tens_res = "forty ";
         break;
     case '5':
-        tens_res = "fivety";
+        tens_res = "fifty ";
         break;
     case '6':
-        tens_res = "sixty";
+        tens_res = "sixty ";
         break;
     case '7':
-        tens_res = "seventy";
+        tens_res = "seventy ";
         break;
     case '8':
-        tens_res = "eightty";
+        tens_res = "eighty ";
         break;
     case '9':
-        tens_res = "ninety";
+        tens_res = "ninety ";
         break;
     default:
+        tens_res = "";
         break;
-}
+    }
+
+    switch(units){
+        case '1':
+            units_res = "one ";
+            break;
+        case '2':
+            units_res = "two ";
+            break;
+        case '3':
+            units_res = "three ";
+            break;
+        case '4':
+            units_res = "four ";
+            break;
+        case '5':
+            units_res = "five ";
+            break;
+        case '6':
+            units_res = "six ";
+            break;
+        case '7':
+            units_res = "seven ";
+            break;
+        case '8':
+            units_res = "eight ";
+            break;
+        case '9':
+            units_res = "nine ";
+            break;
+        default:
+            units_res = "";
+            break;
+    }
+
+    if(thousands_res === "" && hundreds_res === "" && tens_res === "" && units_res === "") return "zero";
 
 
-if(units === "0" && tens !== undefined) return (thousands_res + " " + hundreds_res + " " + tens_res).trim();
-
-
-return (thousands_res + " " + hundreds_res + " " + tens_res + " " + units_res).trim();
+    return (thousands_res  + hundreds_res  + tens_res  + units_res).trim();
 }
